eea4a620e2448cd705bc7d59e820150c975d4e02
Alan Ponte <aponte@Alans-MacBook-Pro.local>
2017-09-12 18:36:38 -0700

    First commit

---
+++ src/main/java/com/alanjponte/jeopardy/JeopardyApplication.java
@@ -0,0 +1,12 @@
+package com.alanjponte.jeopardy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeopardyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeopardyApplication.class, args);
+	}
+}
